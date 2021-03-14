4aa0a5dadba91950bed37857dc64068057851687
EuReinoso <lucas.reinoso.br@gmail.com>
2021-03-14 11:37:09 -0300

    Ordered Vector

---
+++ orderedVector.py
@@ -0,0 +1,30 @@
+import numpy
+from square import Square
+from adjacent import Adjacent
+
+class OrderedVector:
+    def __init__(self,size):
+        self.size = size
+        self.last_position = -1
+        self.valors = numpy.empty(size,dtype=object)
+
+    def insert(self,adj):
+        if self.last_position == self.size -1:
+            print("Full Vector")
+            return
+        
+        pos = 0
+        for i in range(self.last_position + 1):
+            pos = i
+            if self.valors[i].astar_distance > adj.astar_distance:
+                break
+            if i == self.last_position:
+                pos = i + 1 
+        
+        x = self.last_position
+        while x >= pos:
+            self.valors[x+1] = self.valors[x]
+            x-=1
+        
+        self.last_position += 1
+        self.valors[pos] = adj
